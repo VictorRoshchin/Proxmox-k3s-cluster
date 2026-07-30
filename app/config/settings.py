57c7c7799f6976cc2f83d7908784db109a04aed4
Victor Roshchin <microjunior@d-element.ru>
2026-07-30 22:29:40 +0500

    app env

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,20 +1,31 @@
-import environ
+# === IMPORT === #
+
 import os
 
-DEBUG = os.environ.get('DEBUG', default=1)
 
-env = environ.Env()
+# === ENVIRONMENT === #
+
+DEBUG = os.environ.get("DJANGO__DEBUG", "false").lower() == "true"
+SECRET_KEY = os.environ.get("DJANGO__SECRET_KEY", 'django-insecure-00000000000000000000000000000000000000000000000000')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost:8080').split(',')
+ALLOWED_HOSTS = ['*']
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ["DJANGO__DB_HOST"],
+        "PORT": os.environ.get("DJANGO__DB_PORT", "5432"),
+        "NAME": os.environ["DJANGO__DB_NAME"],
+        "USER": os.environ["DJANGO__DB_USER"],
+        "PASSWORD": os.environ["DJANGO__DB_PASSWORD"],
+    }
+}
+TIME_ZONE = os.environ.get("DJANGO__TIME_ZONE", "UTC")
+LANGUAGE_CODE = os.environ.get("DJANGO__LANGUAGE_CODE", "en-us")
+
+
+# === CONFIG === #
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-SECRET_KEY = "django-insecure-3cqe!^9)66b%1j=kglr@9$r%l!2z-kep%4m!t@gr*aa02qs5$&"
-
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost:8080').split(',')
-
-
-ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -56,10 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-DATABASES = {
-    'default': env.db(default='postgresql://postgres:postgres@127.0.0.1:5432/postgres'),
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -75,12 +82,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
 
 STATIC_URL = 'static/'
